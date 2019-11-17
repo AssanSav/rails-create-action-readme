3495573156af2d3612863ad1f4905c78145e698c
Assane Savadogo <assane.savadogo81@gmail.com>
2019-11-16 23:08:51 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,19 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create 
+    #binding.pry
+    @post = Post.create(title: params[:title], description: params[:description])
+    redirect_to post_path(@post)
+  end
+
   def show
     @post = Post.find(params[:id])
   end
 
-  def new
-    @post = Post.new
-  end
-
-  # add create method here
 end
